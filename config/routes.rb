ccf57318cde755fa791eff69ae25f66ef330ef2a
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-08-08 21:02:27 +0500

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users do
+    resources :entities
+    resources :groups
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :groups do
+    resources :entities
+  end
+
+  resources :entities
 end
