60cdf63ddde986b8f5aac6136ff44e063f3ba1ca
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-08-10 02:12:17 +0500

    Add methods for category

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,58 @@
 class GroupsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+  before_action :set_group, only: %i[show update destroy]
+
+  # GET /groups
   def index
-    @groups = Group.all
+    @user = current_user
+    @groups = current_user.groups.includes(:entity).order(id: :desc)
+  end
+
+  def create
+    add_group = current_user.groups.new(group_params)
+    # @group = Group.new(group_params)
+
+    if add_group.save
+      # render json: @group, status: :created, location: @group
+      redirect_to groups_path, notice: 'Category added successfully'
+    else
+      # render json: @group.errors, status: :unprocessable_entity
+      render :new, status: :unprocessable_entity, alert: 'Failed to add category'
+    end
+  end
+
+  # PATCH/PUT /groups/1
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to groups_path, notice: 'Category updated successfully'
+    else
+      render :edit, alert: 'Failed to update category'
+    end
+  end
+
+  # DELETE /groups/1
+  def destroy
+    @user = current_user
+    @group = Group.find(params[:id])
+
+    if @group.destroy
+      redirect_to groups_path, notice: 'Category removed successfully'
+    else
+      render :index, alert: 'Failed to remove category'
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_group
+    @group = Group.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def group_params
+    # params.require(:group).permit(:name, :icon)
+    params.permit(:name, :icon)
   end
 end
