ad61bcd85368727bb764771f803095512e8549bb
fpsapc <112868013+fpsapc@users.noreply.github.com>
2023-08-10 02:12:02 +0500

    Add methods for transactions

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,8 +1,44 @@
 class EntitiesController < ApplicationController
-  before_action :authenticate_user!
-  load_and_authorize_resource
+  before_action :set_entity, only: %i[show update destroy]
 
+  # GET /items
   def index
-    @entities = Entity.all
+    @group = Group.find(params[:group_id])
+    @entities = Entity.where(group_id: @group.id).order(id: :desc)
+    @total_amount = @entities.sum(:amount)
+  end
+
+  # POST /items
+  def create
+    @entities = current_user.item.new(item_params)
+
+    if @entities.save
+      redirect_to group_entities_path, notice: 'Transaction added successfully'
+    else
+      render :new, alert: 'Failed to add transaction'
+    end
+  end
+
+  # DELETE /items/1
+  def destroy
+    @entities = Item.find(params[:id])
+
+    if @entity.destroy
+      redirect_to group_entities_path, notice: 'Transaction removed successfully'
+    else
+      render :index, alert: 'Failed to remove Transaction'
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_entity
+    @item = Item.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def entity_params
+    params.permit(:name, :amount, :group_id)
   end
 end
